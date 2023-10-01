ea8efac41c6e04c6ea88c881a1bdf39b177c009d
toye <106585407+toyeomolara@users.noreply.github.com>
2023-10-01 22:43:10 +0100

    Update assign.js

--- assign.js
+++ assign.js
@@ -3,11 +3,11 @@ Array2 = [5, 6, 3, 10];
 let a = Array1.length;
 let b = Array2.length;
 
-
+ let C = [];
   function divideArray(){
 
      if (a == b){
-        let C = [];
+      
         for (var i = 0; i < Array1.length; i++) {
           C.push(Array1[i] / Array2[i]);
         
