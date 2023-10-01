14673a475b127c30a6b5d0df163f1d846e5d1270
toye <106585407+toyeomolara@users.noreply.github.com>
2023-10-01 22:31:11 +0100

    Initial commit

---
+++ assign.js
@@ -0,0 +1,33 @@
+Array1 = [15, 30, 45, 60];
+Array2 = [5, 6, 3, 10];
+let a = Array1.length;
+let b = Array2.length;
+
+
+  function divideArray(){
+
+     if (a == b){
+        let result = [];
+        for (var i = 0; i < Array1.length; i++) {
+          result.push(Array1[i] / Array2[i]);
+        
+        }
+     }
+   console.log (result)
+    
+  }
+  
+
+
+divideArray() 
+
+
+
+
+
+
+
+
+
+
+ 
